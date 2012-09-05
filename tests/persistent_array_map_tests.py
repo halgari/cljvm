693c37f072ea7f9f939809add9247a2dde3e938a
Timothy Baldridge <tbaldridge@gmail.com>
2012-09-04 22:03:36 -0500

    removing deleted files

--- tests/persistent_array_map_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import system.rt as rt
-from system.core import integer
-
-
-class TestArrayMap(unittest.TestCase):
-    def test_count(self):
-        mp = rt.hash_map.invoke0()
-        assert rt.count.invoke1(mp).int() == 0
